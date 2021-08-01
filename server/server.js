7e89e7ea33ebefa605af1a5520b28df2845fcba0
Wayne Edwards <waedwards42@yahoo.com>
2021-07-31 23:46:41 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 
-
+const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -31,6 +31,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.use(routes)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
